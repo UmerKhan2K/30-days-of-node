2bccc8256e4aa6fcc919608c7d7d91b4cc08a9c1
UmerKhan2K <umerkhannaghar2000@gmail.com>
2023-07-30 00:09:09 +0500

    Day 3

---
+++ Day_3_Regex/Hexadecimal_check.js
@@ -0,0 +1,9 @@
+var str = 'abd'
+var pattern = /^[a-fA-F0-9]+$/g;
+
+var res = str.match( pattern );
+if(res){
+	console.log("Valid Hexadecimal number");
+}else{
+	console.log("Not a valid Hexadecimal number");
+}
---
+++ Day_3_Regex/Valid_pass.js
@@ -0,0 +1,11 @@
+//6-16 length 1 small 1 capital 1 nmbr 1special
+var str = 'Aa#1aaabcde'
+var pattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;;
+
+var res = str.match( pattern );
+if(res){
+	console.log("Valid password");
+}else{
+	console.log("Not a valid password");
+}
+	
