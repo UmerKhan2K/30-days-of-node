f6bac395276e136ff82f6fc15e86c5f880754c2d
UmerKhan2K <umerkhannaghar2000@gmail.com>
2023-07-31 03:52:13 +0500

    Day 4 console

---
+++ Day_4_Console/console_clear.js
@@ -0,0 +1,6 @@
+//Available in Current Version
+var value = 10;
+console.log("Value : %d", value);
+console.clear();//bcz of this 10 is cleared, only 100 is printed
+value *= value;
+console.log("Value : %d", value);	
---
+++ Day_4_Console/console_count.js
@@ -0,0 +1,8 @@
+console.count('default');
+console.count('remo');
+console.count('rj');
+console.count('remo');
+console.count('remo');
+console.count('rj');
+console.count();
+//counts how many time a value is printed
