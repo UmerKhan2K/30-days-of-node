03d04e0cf80d6e3896bf839095c8e1581036759a
UmerKhan2K <umerkhannaghar2000@gmail.com>
2023-08-06 03:59:01 +0500

    crypto_1

---
+++ Day_9_Hashing&HMAC/sha1.js
@@ -0,0 +1,13 @@
+ //Loading the crypto module in node.js
+ var crypto = require('crypto');
+ //creating hash object 
+ var hash = crypto.createHash('sha1');
+ //passing the data to be hashed
+ data = hash.update('nodejsera', 'utf-8');
+ //Creating the hash in the required format
+ gen_hash= data.digest('hex');
+ //Printing the output on the console
+ console.log("hash : " + gen_hash);
+ //Secure Hash Algorithm 1 is a cryptographic hash function which generates
+ // a hash value which is typically rendered as a hexadecimal number of exactly 40 digits long. 
+ //It produces a 160-bit hash value which is known as message digest.
